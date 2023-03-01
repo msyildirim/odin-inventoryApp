3a3e8db5c803e7694675477563386dff797a26b6
msyildirim <msy.90@hotmail.com>
2023-03-01 13:44:24 +0300

    main page basic version

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ var app = express();
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
-const mongoDB = "mongodb+srv://muhammetconsole2:AjvVnbbiY8Em2ZEl@cluster0.c6kcog8.mongodb.net/dealer?retryWrites=true&w=majority";
-
+const mongoDB = 
 main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
